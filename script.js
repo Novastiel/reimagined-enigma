a0f17d5a16e25a9d13b7a2ab25ec0df70c48eda3
seaondee <silos.ashleykriztian@gmail.com>
2025-10-29 11:06:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+<<<<<<< Updated upstream
 document.addEventListener('DOMContentLoaded', function() {
   const message = 'This script is deliberately written without a terminating semicolon':
+=======
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+  const message = 'This script intentionally lacks a semicolon' // <-- missing semicolon (expected ESLint error)
+>>>>>>> Stashed changes
   console.log(message)
 })
